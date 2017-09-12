4c4381c06e466e07d7bc1383c9e80bf1821ab901
webbbx <webbbx@sina.com>
2017-09-12 16:41:35 +0800

    Update reverseip.py

--- reverseip.py
+++ reverseip.py
@@ -10,21 +10,31 @@ import requests,json,urllib,sys,os
 from bs4 import BeautifulSoup  
 import re
 #获取页面内容  
-def getPage(ip):  
+def get_Domain(ip):  
     url="http://dns.aizhan.com/%s/" % (ip)
-    response=urllib.urlopen(url)
-    data=response.read()
-    souphtml=BeautifulSoup(data)
-    domainList = souphtml.find_all(text=re.compile("(\w+.\w.(?:com\.cn|cn|com|net))"))
-    # print domainList
+    try:
+        response=urllib.urlopen(url)
+        data=response.read()
+        souphtml=BeautifulSoup(data)
+        # domainList = souphtml.find_all(text=re.compile("(\w+.\w.(?:com\.cn|cn|com|net))"))
+        domainList = souphtml.find_all(text=re.compile("^((?!http).)*com|.cn|.net|.org|.gov|.edu|.wang"))
+        # print domainList
+        domainList_Make = domainList[:-1]
+    except Exception,e:
+        print e
+        print "%s can not be reversed" % ip
+
     # domainList2 = ",".join(domainList).encode('utf-8')
-    for i in range(0,10):
-        print domainList[i].encode('utf-8')
+    try:
+        for i in domainList_Make:    
+            print i.encode('utf-8')
+    except Exception,e:
+        print "%s can not be reversed" % ip
+        
 
 if __name__ == "__main__":  
-#     ip='60.190.243.200'
-#     outfile='F:/python1/saomiaoqi/1.html'
+    # ip='162.159.209.34'
     ip = str(sys.argv[1])  
     print "The target IP is :%s" % ip  
     print "Starting, please wait..."  
-    getPage(ip)
+    get_Domain(ip)
